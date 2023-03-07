62bc06f356596cd7cff328c1e0fe8aad3d5ba7f5
Gino Torres Jr <gindogg878@icloud.com>
2023-03-07 15:59:03 -0600

    adding last function

--- functions-git.js
+++ functions-git.js
@@ -8,21 +8,20 @@ const minusOne = function (num) {
 
 console.log(minusOne("Infinity"));
 
+//second functions//
 const makeSentence = function (one, two, three) {
   console.log(`${one} ${two} ${three}`);
 };
 
+makeSentence("Me", "likey", "pizza!");
+
+//3 function
 const getLastElement = function (arr) {
   return arr[arr.length - 1];
 };
 
-getLastElement([1, 2, 3, 4, 5, 6]);
-getLastElement(["a", "b", "c"]);
-getLastElement([
-  [1, 2, 3],
-  [4, 5, 6],
-]);
-
+console.log(getLastElement([1, 2, 3, 4, 5, 6]));
+console.log(getLastElement(["a", "b", "c"]));
 console.log(
   getLastElement([
     [1, 2, 3],
